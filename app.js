026e6975eea29a4901b47cd0aff6a848e8dbbaff
sachin-me <sachinkumartiwari363@gmail.com>
2021-08-17 01:01:34 +0530

    Setup repo and added boiler plate

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const session = require("express-session");
+const app = express();
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+
+const port = process.env.PORT;
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err, connection) {
+    if (err) throw err;
+    else console.log("connected to mongodb");
+  }
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "./server/views"));
+app.set("view engine", "ejs");
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongoUrl: process.env.MONGO_SESSION_URI }),
+  })
+);
+
+if (process.env.NODE_ENV === "development") {
+  var webpack = require("webpack");
+  var webpackConfig = require("./webpack.config");
+  var compiler = webpack(webpackConfig);
+
+  app.use(
+    require("webpack-dev-middleware")(compiler, {
+      publicPath: webpackConfig.output.publicPath,
+    })
+  );
+
+  app.use(require("webpack-hot-middleware")(compiler));
+}
+
+app.use(cors());
+
+app.use("/api", require("./server/routes/api"));
+app.use(require("./server/routes/index"));
+
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
+});
---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Welcome to the world of programming</p>
+      </div>
+    );
+  }
+}
+
+export default App;
