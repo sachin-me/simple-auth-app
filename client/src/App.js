72381ccf58863c48b7dcc8c99d192eda3ea57029
sachin-me <sachinkumartiwari363@gmail.com>
2021-08-17 02:57:12 +0530

    Add static signup form

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import "./App.scss";
 import Header from "./components/Header";
+import Signup from "./components/Signup";
 
 function App(props) {
   return (
     <div className="App">
       <Router>
         <Header />
+        <Switch>
+          <Route path="/signup" component={Signup} />
+        </Switch>
       </Router>
     </div>
   );
---
+++ client/src/components/Signup/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignupForm from "./SignupForm";
+
+function Signup(props) {
+  return (
+    <div className="form-wrapper">
+      <SignupForm />
+    </div>
+  );
+}
+
+export default Signup;
