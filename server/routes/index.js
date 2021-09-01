644f6aa5d503c0c64bff6a5f9ccc0e03b7f73bbb
sachin-me <sachinkumartiwari363@gmail.com>
2021-09-01 15:31:03 +0530

    fixed minor issue

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,11 +5,11 @@ const hash = "bundle";
 router.get("*", function(req, res, next) {
 	const cssPath =
 		process.env.NODE_ENV == "production"
-			? `/bundle/${hash}.css`
+			? `/dist/bundle/${hash}.css`
 			: "/static/bundle.css";
 	const jsPath =
 		process.env.NODE_ENV == "production"
-			? `/bundle/${hash}.js`
+			? `/dist/bundle/${hash}.js`
 			: "/static/bundle.js";
 	res.render("index", { jsPath, cssPath });
 });
