4b6fdee4d179c1943c9937b0f23b3f9a79918c1c
sachin-me <sachinkumartiwari363@gmail.com>
2021-09-01 13:06:20 +0530

    Added production configuration

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const hash = "bundle";
 
-router.get("*", (err, res) => {
-  res.render("index");
+router.get("*", function(req, res, next) {
+	const cssPath =
+		process.env.NODE_ENV == "production"
+			? `/bundle/${hash}.css`
+			: "/static/bundle.css";
+	const jsPath =
+		process.env.NODE_ENV == "production"
+			? `/bundle/${hash}.js`
+			: "/static/bundle.js";
+	res.render("index", { jsPath, cssPath });
 });
 
 module.exports = router;
